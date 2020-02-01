f4fd8651dab2313922ee368123875c14d2df7937
Better-Boy <panaroma365@gmail.com>
2020-02-01 20:08:53 +0530

    First Commit
    added to generate the tree and see the visualization

---
+++ tree_generator.py
@@ -0,0 +1,50 @@
+from anytree import Node, RenderTree, LoopError
+from anytree.exporter import JsonExporter
+import json
+import argparse
+
+EXPORTER = JsonExporter(indent=2, sort_keys=True)
+NODE_DICT = {}
+ERROR_RECORDS = []
+
+def cmd_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputFilePath", required=True, help="Input Text File that contains new line delimited breadcrumbs", dest="inputFile")
+    parser.add_argument("--sep", required=True, help="Separator between the levels of the breadcrumbs. Example. '-','/'", dest="sep")
+    parser.add_argument("--nodetoexport", required=True, help="Parent Node name and it's children to export to json", dest="node_name")
+    parser.add_argument("--outputFilePath", help="Output file to export for visualization", dest="outputFile", default="data.json")
+    return parser.parse_args()
+
+def read_input_file(fpath):
+    return open(fpath).readlines()
+
+def build_tree(input_breadcrumbs, separator):
+    for path in input_breadcrumbs:
+        try:
+            path_nodes = path.strip().split(separator)
+            path_nodes_tuple = [(path_nodes[h].strip(), path_nodes[h+1].strip()) for h in range(0, len(path_nodes)-1)]
+            for parent, child in path_nodes_tuple:
+                if parent not in NODE_DICT.keys():
+                    NODE_DICT[parent] = Node(parent)
+                if child not in NODE_DICT.keys():
+                    NODE_DICT[child] = Node(child)
+                NODE_DICT[child].parent = NODE_DICT[parent]
+        except Exception as exc:
+            print("Error - " + exc)
+            ERROR_RECORDS.append({"record":path, "error_message": exc})
+
+def export_node(node_name, outputFileName):
+    EXPORTER.write(NODE_DICT[node_name], open(outputFileName, "w"))
+
+def save_error_records():
+    json.dump(ERROR_RECORDS, open("error_records.json", "w"))
+
+def start():
+    conf = vars(cmd_line_args())
+    breadcrumbs = read_input_file(conf["inputFile"])
+    build_tree(breadcrumbs, conf["sep"].strip())
+    export_node(conf["node_name"], conf["outputFile"])
+    save_error_records()
+
+if __name__ == "__main__":
+    start()
